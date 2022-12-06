158e4695f61c432d907e812f0af7fdcc0f3d1ce2
JodieEtFreXys~ <jodietyaswara2@gmail.com~>
2022-12-06 23:50:02 +0700

    Second commit

---
+++ view/shopList.php
@@ -0,0 +1,40 @@
+<?php
+
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrapper">
+
+        <img class="logo-dropdown" src="/assets/images/image 9.svg" alt="">
+        
+        <div class="dropdown">
+            <a href="">Weekly Analytics</a>
+            <a href="">Help</a>
+            <a href="">Privacy Policy</a>
+            <a href="">About Us</a>
+            <a href="">Login</a>
+            <a href="">Logout</a>
+            <a href="">Register</a>
+        </div>
+    </div>
+
+    <div class="topnav">
+        <a class="uncurr-page" href="/view/main.php">Stock</a>
+        <a class="curr-page" href="/view/shopList.php">Shopping List</a>
+        <a class="uncurr-page" href="/view/recipe.php">Recipe</a>
+        <a class="uncurr-page" href="/view/expired.php">Expired</a>
+    </div>
+</body>
+</html>
