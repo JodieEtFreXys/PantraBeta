672eaf0b0df293cedaabd7ad623d5dfa0027186c
JodieEtFreXys~ <jodietyaswara2@gmail.com~>
2022-12-07 22:37:39 +0700

    Fourth commit

--- view/login.php
+++ view/login.php
@@ -2,9 +2,14 @@
 
     session_start();
 
-    include ('../model/connection.php');
+    // include ('../model/connection.php');
+    include ('../controller/inputSanitize.php');
     
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!sanitizeEmail($_POST['email'])) {
+            die;
+        }
+        
         
     }
 
@@ -24,7 +29,7 @@
         <img class="logo-pantrana" src="/assets/images/Logo B.svg" alt="">
         <h2>LOGIN</h2>
 
-        <form action="" class="login-form">
+        <form action="" class="login-form" method="POST">
             <input type="text" placeholder="Email" name="email">
             <input type="text" placeholder="Password" name="password">
             <button class="login-btn" type="submit">LOGIN</button>
